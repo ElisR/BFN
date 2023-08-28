75128e24728abdd509cba8bd95fa9f98d45ef6bf
Elis Roberts <elisiforroberts@gmail.com>
2023-08-28 09:01:40 +0100

    test: :white_check_mark: Add basic tests for loss function and sampling
    Only testing whether they run and give correct shapes. Will split up tests. Also want to run beartype during tests.

---
+++ test_categorical.py
@@ -0,0 +1,32 @@
+import jax.numpy as jnp
+import jax.random as jr
+import pytest
+
+import train_and_sample as tas
+
+# TODO Make fixture and split up unit tests
+
+
+@pytest.mark.parametrize(("k", "d"), [(5, 10), (10, 5), (1, 1)])
+def test_discrete_output_distribution(k: int, d: int):
+    dod = tas.DiscreteOutputDistribution(k, d)
+
+    # Test distribution
+    key, subkey1, subkey2 = jr.split(jr.PRNGKey(0), 3)
+    thetas_example = jr.uniform(subkey1, (k, d))
+    variables = dod.init(subkey2, thetas_example, 0.5)
+    out = dod.apply(variables, thetas_example, 0.5)
+
+    assert isinstance(out, jnp.ndarray)
+    assert out.shape == (k, d)
+
+    # Test loss
+    key, subkey1, subkey2 = jr.split(key, 3)
+    x = jr.randint(subkey1, shape=(d,), minval=0, maxval=k)
+    loss_eval = tas.loss(x, variables, dod, 0.5, key=subkey2)
+    assert loss_eval.shape == ()
+
+    # Test sampling
+    key, subkey = jr.split(key)
+    final_sample = tas.sample(variables, dod, 0.5, 100, key=subkey)
+    assert final_sample.shape == (d,)
