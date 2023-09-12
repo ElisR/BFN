1bdc49dc5db26fa35241a96fb01faa2b3768989b
Elis Roberts <elisiforroberts@gmail.com>
2023-09-12 08:17:58 +0100

    perf: :zap: Update continuous flow visualisation to use probability directly
    More efficient than deducing probability from many samples.

--- bfn/continuous/visualising_flows.py
+++ bfn/continuous/visualising_flows.py
@@ -8,7 +8,7 @@ from jaxtyping import Float, Int, Key, Array
 
 
 @partial(jax.jit, static_argnums=(2,), static_argnames=("bins", "time_slices"))
-def flow(x: Float[Array, "D"], sigma_1: float, samples: int, *, key: Key, mu_range: tuple[float], bins: int = 1000, time_slices: int = 100):
+def flow_old(x: Float[Array, "D"], sigma_1: float, samples: int, *, key: Key, mu_range: tuple[float], bins: int = 1000, time_slices: int = 100):
     r"""Calculate the flow distribution for a certain observation as a histogram.
 
     This is an inefficient way to do it because Gaussian probability is easily evaluated.
@@ -37,6 +37,37 @@ def flow(x: Float[Array, "D"], sigma_1: float, samples: int, *, key: Key, mu_ran
 
     # Using lambda function because of kwargs
     return jax.vmap(lambda y_t: jnp.histogram(y_t, bins=bins, density=True, range=mu_range))(ys)
+
+
+@partial(jax.jit, static_argnames=("bins", "time_slices"))
+def flow(x: Float[Array, "D"], sigma_1: float, *, mu_range: tuple[float], bins: int = 1000, time_slices: int = 100):
+    """Calculate the flow distribution for a certain observationi as a histogram.
+    
+    More efficient way than the old way.
+
+    Args:
+        x: The observation to calculate the flow distribution for.
+        sigma_1: The standard deviation of the t=1 Gaussian.
+        samples: The number of samples to use to calculate the flow distribution.
+        mu_range: The range of mu values to use for the histogram.
+        bins: The number of bins to use for the histogram.
+        time_slices: The number of time slices to use for the flow distribution.
+
+    Returns:
+        The flow distribution as histogram:
+        Densities of shape (time_slices, bins) and edges of shape (bins,)
+    """
+    ts = jnp.linspace(0, 1, time_slices)
+    gamma_ts = 1 - jnp.power(sigma_1, 2 * ts)
+    gamma_ts = jnp.expand_dims(gamma_ts, (1,))
+
+    mus = jnp.linspace(*mu_range, num=bins)
+    mus = jnp.expand_dims(mus, (0,))
+
+    x = jnp.expand_dims(x, (0,))
+    ps = jnp.exp(-0.5 * jnp.square((mus - gamma_ts * x) / jnp.sqrt(gamma_ts * (1 - gamma_ts)))) / jnp.sqrt(2 * jnp.pi * gamma_ts * (1 - gamma_ts))
+    ps = jnp.nan_to_num(ps, nan=0.0)
+    return ps, mus[0]
 
 
 @partial(jax.jit, static_argnums=(2,))
